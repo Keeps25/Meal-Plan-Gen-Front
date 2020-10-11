e7a513abe0f1b984e094bb09234e7ed307a01d21
ritvik7 <ritvikgaur7@gmail.com>
2020-10-10 23:42:40 -0700

    carousel and input text

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Button from 'react-bootstrap/Button'
+import Carousel from 'react-bootstrap/Carousel'
+import image1 from './assets/img/image1.png'
+import image2 from './assets/img/image2.jpg'
+import image3 from './assets/img/image3.jpg'
+import image4 from './assets/img/image4.jpg'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
-function App() {
+const App = () => {
+  const [calCount, setCalCount] = useState()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <Jumbotron className='jumbo'>
+        <h1>Meal Plan Site</h1>
+        <div className='cal-input'>
+          <input className='cal-field' type='text' onvalue={calCount}
+            placeholder="What's your calorie goal?"
+            onChange={event => setCalCount(event.target.value)}/>
+        </div>
+        <Button variant='outline-light' className="btn" as="input" type="submit" value="Submit" />{' '}
+      </Jumbotron>
+      <Carousel controls={false} interval={5000} pause={false}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={image1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Feature one</h3>
+          <p>Descirbe Feature</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={image2}
+          alt="Second slide"
+        />
 
+        <Carousel.Caption>
+          <h3>Second feature</h3>
+          <p>Describe feature</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={image3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third feature</h3>
+          <p>
+            Describe feature
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={image4}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Fourth feature</h3>
+          <p>
+            Describe feature
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
+  )
+}
 export default App;
